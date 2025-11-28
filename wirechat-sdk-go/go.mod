6d19bcad9df05e07fdb43d830190db542cc600c9
vova.kirdan@mail.ru <_4IDuzhSOe>
2025-11-28 21:09:31 +0300

    Add initial project structure for wirechat SDKs in C, Go, Python, and Rust
    - Created .gitignore files for C and Go SDKs to exclude build artifacts.
    - Added go.mod and go.sum for Go SDK dependency management.
    - Initialized Python SDK with main.py, .python-version, and pyproject.toml.
    - Set up Rust SDK with Cargo.toml and a basic main.rs file.

---
+++ wirechat-sdk-go/go.mod
@@ -0,0 +1,5 @@
+module wirechat-sdk-go
+
+go 1.25.2
+
+require nhooyr.io/websocket v1.8.17 // indirect
---
+++ wirechat-sdk-python/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from wirechat-sdk-python!")
+
+
+if __name__ == "__main__":
+    main()
